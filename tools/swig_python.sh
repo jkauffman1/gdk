6ea30b8015a404e40614660b907fd4d2094be198
Jeremy Kauffman <jeremy@blockstream.io>
2020-01-31 12:16:12 +0000

    wip

--- tools/swig_python.sh
+++ tools/swig_python.sh
@@ -7,11 +7,22 @@ outdir=$3
 swig_src=$4
 swig_extra=$5
 
+echo **** swig_python.sh
+echo **** sed_exe = $sed_exe
+echo **** generated_c = $generated_c
+echo **** outdir = $outdir
+echo **** swig_src = $swig_src
+echo **** swig_extra = $swig_extra
+
 module_dir="$outdir/greenaddress"
 mkdir -p $module_dir
 
+echo **** module_dir = $module_dir
+
 swig -python -py3 -threads -o $generated_c -outdir $outdir -o $generated_c $swig_src
 swig_result="$outdir/greenaddress.py"
+
+echo ***** swig_result = $swig_result
 
 # Fix up shared library names
 $sed_exe -i 's/_greenaddress/libgreenaddress/g' $generated_c $swig_result
